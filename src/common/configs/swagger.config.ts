ee6a3b52bc64cf6f07cb1349fc6f5c5b81779c2c
_lmmmmmm <931420654@qq.com>
2022-04-28 00:54:32 +0800

    chore(swagger): update swagger config

--- src/common/configs/swagger.config.ts
+++ src/common/configs/swagger.config.ts
@@ -1,7 +1,13 @@
 import { DocumentBuilder } from '@nestjs/swagger';
 
 export const swaggerConfig = new DocumentBuilder()
-  .setTitle('Application Api Docs')
-  .setDescription('Application Api Docs Description')
-  .setVersion('0.1')
+  .setTitle('Malo Putovanja Api 接口文档')
+  .setDescription(
+    'Malo Putovanja git address: https://github.com/Lmmmmmm-bb/Malo-Putovanja',
+  )
+  .setLicense(
+    'Prinz use MIT LICENSE',
+    'https://github.com/Lmmmmmm-bb/Prinz/blob/main/LICENSE',
+  )
+  .setVersion('0.0.1')
   .build();
