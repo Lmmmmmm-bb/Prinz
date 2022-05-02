1c9a56543d4bb1f80e05f4ac5e6d604c82fcafa9
_lmmmmmm <931420654@qq.com>
2022-05-02 12:45:33 +0800

    fix(authorization): jwt parse error

--- src/common/guards/auth.guard.ts
+++ src/common/guards/auth.guard.ts
@@ -5,24 +5,18 @@ import {
   Injectable,
 } from '@nestjs/common';
 import { AuthService } from 'src/modules/auth/auth.service';
+import { parseToken } from '../utils/jwt.util';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(@Inject(AuthService) private readonly authService: AuthService) {}
-
-  private getToken(authorization: string) {
-    if (authorization.startsWith('Bearer')) {
-      return authorization.split(' ')[1];
-    }
-    return authorization;
-  }
 
   canActivate(ctx: ExecutionContext) {
     const request = ctx.switchToHttp().getRequest();
     const { authorization } = request.headers;
 
     try {
-      const token = this.getToken(authorization);
+      const token = parseToken(authorization);
       this.authService.transform(token);
     } catch {
       return false;
